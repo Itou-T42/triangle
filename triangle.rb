262656e019fa75723f0690d9ab37ae47f28007e5
Itou-T42 <Itou-T42@mail.dnp.co.jp>
2015-08-12 17:02:29 +0900

    RSpecでテストケースを実装

--- triangle.rb
+++ triangle.rb
@@ -2,14 +2,21 @@ a = ARGV[0].to_i
 b = ARGV[1].to_i
 c = ARGV[2].to_i
 
-if a + b > c && b + c > a && c + a > b
-  if a == b && a == c && c == a
-    puts "正三角形ですね！"
-  elsif a == b || b == c || c == a
-    puts "二等辺三角形ですね！"
-  else
-    puts "不等辺三角形ですね！"
+class Triangle
+  def self.display(a,b,c)
+    if a + b > c && b + c > a && c + a > b
+      if a == b && a == c && c == a
+        return "正三角形ですね！"
+      elsif a == b || b == c || c == a
+        return "二等辺三角形ですね！"
+      else
+        return "不等辺三角形ですね！"
+      end
+    else
+      return "三角形じゃないです＞＜"
+    end
   end
-else
-  puts "三角形じゃないです＞＜"
 end
+
+result = Triangle.display(a,b,c)
+puts result
